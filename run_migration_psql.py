bba567e138329c90349e256d2fafc168b1902cf4
tompa <tompalmer@live.co.uk>
2026-05-05 21:25:01 +0100

    run migration psql

---
+++ run_migration_psql.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Repo-root convenience wrapper for scripts/run_migration_psql.py."""
+
+from scripts.run_migration_psql import main
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
